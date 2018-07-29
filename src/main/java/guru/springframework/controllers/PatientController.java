ece7cc299ec67bcc420457f778ed9e9f19dc73a2
Shihang Wang <ucabsw2@ucl.ac.uk>
2018-07-29 14:21:24 +0100

    retrieve patient info from facade

--- src/main/java/guru/springframework/controllers/PatientController.java
+++ src/main/java/guru/springframework/controllers/PatientController.java
@@ -1,6 +1,7 @@
 package guru.springframework.controllers;
 
 
+import guru.springframework.domain.Patient;
 import org.springframework.stereotype.Controller;
 import guru.springframework.conversion.PatientConversion;
 import org.springframework.ui.Model;
@@ -9,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import guru.springframework.conversion.RetrieveData;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class PatientController {
@@ -19,21 +21,26 @@ public class PatientController {
     }
 
 //<<<<<<< HEAD
-//    @RequestMapping("/patient/new")
+//    @RequestMapping("/patient")
 //    public String newPatient(Model model){
 //        model.addAttribute("patient", new Patient());
-//        System.out.println(this);
+//        System.out.println("我心态崩了");
 //        return "patientform";
 //    }
-//
+
 //    @RequestMapping(value = "/patient", method = RequestMethod.POST)
-//        public String saveProduct(Patient patient){
+//    public String saveProduct(Patient patient){
+//        return "";
+//    }
 
     @RequestMapping("/patient/{id}")
-    public String convertPatient(@PathVariable Long id, Model model){
+    public String convertPatient(@PathVariable Long id){
         RetrieveData temp = new RetrieveData("http://localhost:8080/api/patients/"+id);
-        model.addAttribute(temp.ConvertResponse());
-        return "patientResource";
+        System.out.println(temp.ConvertResponse());
+//        model.addAttribute("patient", temp.ConvertResponse());
+        System.out.println("我心态崩了aaaaaaa");
+
+        return "index";
     }
 
 
